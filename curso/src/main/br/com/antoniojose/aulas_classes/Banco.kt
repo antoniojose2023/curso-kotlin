e381ac5a7aa8a587596cb90d2d87e35b5d645415
antoniojose2023 <antoniojoseuchoa2023@gmail.com>
2022-01-09 07:36:03 -0300

    Criando data Class Banco

---
+++ curso/src/main/br/com/antoniojose/aulas_classes/Banco.kt
@@ -0,0 +1,9 @@
+package main.br.com.antoniojose.aulas_classes
+
+data class Banco(
+      val nome: String,
+      val numero: Int
+) {
+
+
+}
---
+++ curso/src/main/br/com/antoniojose/aulas_classes/Testes.kt
@@ -0,0 +1,17 @@
+package main.br.com.antoniojose.aulas_classes
+
+fun main(){
+
+    var digitalOneBank = Banco(nome = "OneBank", numero = 10)
+
+
+    println(digitalOneBank.nome)
+    println(digitalOneBank.numero)
+
+
+    val filial = digitalOneBank.copy("Banco filial")
+    println(filial.nome)
+    println(filial.numero)
+
+}
+
