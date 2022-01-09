1a8bf948a1bab21479cd2e86b1d7df97605a3906
antoniojose2023 <antoniojoseuchoa2023@gmail.com>
2022-01-08 23:05:34 -0300

    Criando da classe conta

---
+++ curso/src/main/br/com/antoniojose/aulas_classes/Conta.kt
@@ -0,0 +1,19 @@
+package main.br.com.antoniojose.aulas_classes
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+
+    fun depositar(deposito: BigDecimal){
+
+    }
+
+    fun sacar(saque: BigDecimal){
+
+    }
+
+}
